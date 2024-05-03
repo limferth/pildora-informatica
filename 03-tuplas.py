0f304e1be3a7535b9680f4e85e5e0826151ecf73
limferth <limferth@gmail.com>
2024-05-02 22:05:49 -0400

    clases de tuplas y diccionarios a media la ultima

---
+++ 03-tuplas.py
@@ -0,0 +1,26 @@
+# LAS TUPLAS
+
+# Son  listas inmutables, es decir que no se pueden modificar de su creacion.
+
+miTupla = ("Juan", 12, 1, 1995)
+
+print(miTupla[2])
+
+#listas en tuplas y tuplas en listas--> (), listas-->[]
+
+miLista=list(miTupla)
+
+print(miLista)
+
+#METOS QUE APRENDIMOS
+#tuple, count, len, 
+
+#miTuplas=("juan",)
+#print(len(miTuplas))  #es tupla unitaria
+
+miFecha=("juanito", 25, 1, 1199)
+nombre, dia, mes, agno=miFecha
+print(nombre)
+print(dia)
+print(mes)
+print(agno)
---
+++ 04-diccionarios.py
@@ -0,0 +1,12 @@
+#LOS DICCIONARIOS
+
+#Son estructuras de datos que nos permiter almacenar valor  de diferenters tipos, (Enteros, cadenas de texto, decimales) e incluso listas y otros diccionarios.
+
+# la  principal caracteristica de los diccionarios es que los datos se almacenan asociados a una clave de tal forma que mse crea una asociacion de ttipo CLAVE:VALOR para cada elemento almacenado.
+
+#los elementos almacenados no estan ordenados, el orden es indiferente a la hora de almacenar informacion en un  diccionario.
+
+
+miDiccionario={"Alemania":"Berlin", "Francia":"paris", "uk":"london", "espania":"Madrid"}
+
+print(miDiccionario["Francia", "espania"])
